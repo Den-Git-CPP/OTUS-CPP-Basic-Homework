9d4eab5c33b7482d719928c7df18802c34a9494c
Den-Git-CPP <madim-neodart@rambler.ru>
2021-11-05 14:27:03 +0300

    Изменен print_table_score для вывода лучших результатов

--- homework_3/homework_3_ver_2/print_table_score.cpp
+++ homework_3/homework_3_ver_2/print_table_score.cpp
@@ -2,10 +2,14 @@
 #include <fstream>
 #include <string>
 
+#include <algorithm>
+#include <vector>
+
 #include "print_table_score.h"
 
 void print_table_score(const std::string& table_path)
 {
+	
 	// Read the high score file and print all results
 	{
 		std::ifstream in_file{ table_path };
@@ -18,18 +22,50 @@ void print_table_score(const std::string& table_path)
 
 		std::string username;
 		int high_score = 0;
+		std::vector<std::pair<std::string, int>> table;
+
+		table.reserve(5);
+
 		while (true) {
 
 			in_file >> username >> high_score;
 			// Ignore the end of line symbol
 			in_file.ignore();
 
-			if (in_file.fail()) {
-				break;
+
+			auto it = std::find_if(table.begin(), table.end(),
+				[&](const auto& item) {
+					return std::get<0>(item) == username;
+				});
+
+			if (it != table.end()) {
+				// found username!
+				
+				if (std::get<1>(*it) > high_score)
+				{
+					table.erase(it);
+					table.push_back(make_pair(username, high_score));
+					
+				};
+			}
+			else {
+				//pair not found
+				table.push_back(make_pair(username, high_score));
+
 			}
 
-			// Print the information to the screen
-			std::cout << username << '\t' << high_score << std::endl;
+
+
+			if (in_file.fail()) {
+				break;
+			};
+
+		};
+
+
+		for (auto elem : table) {
+			std::cout << elem.first << " " << elem.second << std::endl;
 		}
+
 	}
 }
