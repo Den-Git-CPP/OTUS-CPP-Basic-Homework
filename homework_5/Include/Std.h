067addd1355b7c64a359e797e4919c96bd62fc08
Den-Git-CPP <madim-neodart@rambler.ru>
2021-11-13 10:25:36 +0300

    Добавлен p_vect() Исправлена ошибка счета

--- homework_5/Include/Std.h
+++ homework_5/Include/Std.h
@@ -2,14 +2,14 @@
 #include <iostream>
 #include "IStatistics.h"
 #include <cmath>
+#include <vector>
 
 class Std : public IStatistics {
 public:
-	Std() : m_std{0}, m_sum{0}, m_count{1} {}
+	Std() : m_std{ 0 }, m_sum{ 0 }, m_count{ 0 } {}
 	void update(double next) override;
 	double eval() const override;
 	const char* name() const override;
-	
 private:
 	double m_std,
 		m_mean,
@@ -17,5 +17,6 @@ private:
 		m_count,
 		m_std_val,
 		m_disp = 0;
-
+	void p_vect();
+	std::vector<double>m_vect_dbl;
 };
--- homework_5/Src/Std.cpp
+++ homework_5/Src/Std.cpp
@@ -1,24 +1,38 @@
 
 #include "Std.h"
 
+void Std::p_vect()
+{
+	m_sum = 0;
+	m_mean = 0;
+	m_disp = 0;
+	m_std_val = 0;
+
+	for (int i = 0; i < m_count; ++i) {
+		m_sum += m_vect_dbl.at(i);
+	}
+	m_mean = m_sum / m_count;
+
+	for (int i = 0; i < m_count; ++i) {
+		m_disp += pow(m_vect_dbl.at(i) - m_mean, 2);
+	}
+	m_std_val = sqrt(m_disp / m_count);
+
+}
+
 void Std::update(double next)
 {
-		m_sum += next;
-		m_mean = m_sum / m_count;
-		m_disp += pow((next - m_mean), 2) / m_count;		 
-		m_std_val = sqrt(m_disp);							 
-		m_count++;
-
-	}
+	m_vect_dbl.push_back(next);
+	m_count++;
+	p_vect();
+}
 
 double Std::eval() const
 {
+
 	return m_std_val;
 }
-
 const char* Std::name() const
 {
 	return "Std";
 }
-
-
